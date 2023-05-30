b89f1e91ab884806a09dd45c343097c656b20128
Humble <humble@example.com>
2023-05-30 14:22:35 +0200

    moving launch files to python

---
+++ launch/simbot/simbot_launch.py
@@ -0,0 +1,131 @@
+import os
+
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.actions import DeclareLaunchArgument, SetEnvironmentVariable
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+from nav2_common.launch import RewrittenYaml
+from ament_index_python.packages import get_package_share_directory
+
+
+def generate_launch_description():
+
+    # Get the pkg directory
+    pkg_dir = get_package_share_directory('missions_pkg')
+
+    # variables/files
+    use_sim_time = True
+    remappings = []
+    params_yaml_file = os.path.join(pkg_dir, 'launch', 'simbot', 'rhodon_params.yaml')
+    nav2_launch_file = os.path.join(pkg_dir, 'launch', 'simbot', 'nav2_launch.py')
+    rviz_file = os.path.join(pkg_dir, 'rviz', 'coppelia_sim.rviz')
+    urdf = os.path.join(pkg_dir, 'launch', 'rhodon', 'rhodon_urdf.xml')
+    with open(urdf, 'r') as infp:
+        robot_desc = infp.read()
+    logger = LaunchConfiguration("log_level")
+    
+    return LaunchDescription([
+        # Set env var to print messages to stdout immediately
+        SetEnvironmentVariable('RCUTILS_LOGGING_BUFFERED_STREAM', '1'),
+        
+        # Declare Arguments 
+        # ==================
+        DeclareLaunchArgument(
+            "log_level",
+            default_value=["info"],  #debug, info
+            description="Logging level",
+            ),
+
+        # HW_DRIVERS
+        #============
+        # Simulator (CoppeliaSIM)
+        # See simulator.launch
+                        
+        # Keyboard Control
+        Node(
+            package='keyboard_control',
+            executable='keyboard_control_plus',
+            name='keyboard_control',
+            output='screen',
+            prefix="xterm -hold -e",
+            parameters=[params_yaml_file]
+            ),
+
+        # MANAGEMENT
+        #===========
+        # Task Manager
+        Node(
+            package='task_manager',
+            executable='bt_manager',
+            name='task_manager',
+            output='screen',
+            prefix="xterm -hold -e",
+            parameters=[params_yaml_file]
+            ),
+        # Task From Topic
+        Node(
+            package='task_manager',
+            executable='task_from_topic',
+            name='task_from_topic',
+            output='screen',
+            prefix="xterm -hold -e",
+            parameters=[params_yaml_file]
+            ),
+        
+        # UTILS
+        #=========
+        # NAV2 Autonomous Navigation (see nav2_launch.py)
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(nav2_launch_file)
+        ),
+
+        # MQTT_bridge
+        Node(
+            package='mqtt_bridge',
+            executable='mqtt_bridge_node',
+            name='mqtt_bridge',
+            output='screen',
+            prefix='xterm -hold -e',
+            parameters=[params_yaml_file]            
+            ),
+
+        # HRI
+        Node(
+            package='web_hri',
+            executable='web_hri',
+            name='web_hri',
+            output='screen',
+            prefix='xterm -hold -e',
+            parameters=[params_yaml_file]
+            ),
+
+        # PATROL
+        Node(
+            package='patrol',
+            executable='patrol_times',
+            name='patrol_times',
+            output='screen',
+            parameters=[params_yaml_file]
+            ),
+        
+        # Battery_Manager
+        Node(
+            package='battery_manager',
+            executable='battery_manager',
+            name='battery_manager',
+            output='screen',
+            prefix="xterm -hold -e",
+            parameters=[params_yaml_file]            
+            ),
+        
+        # Status Publisher
+        Node(
+            package='robot_status_publisher',
+            executable='robot_status_publisher_node',
+            name='status_publisher',
+            output='screen',
+            parameters=[params_yaml_file]
+            )
+    ]) #end LaunchDescription
