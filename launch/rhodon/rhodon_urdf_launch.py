e9f4e06fc616275994f11350aa4161a8e6d9431c
Pepe Ojeda <ojedamorala@hotmail.com>
2023-07-11 10:28:45 +0200

    updated launch files

---
+++ launch/rhodon/rhodon_urdf_launch.py
@@ -0,0 +1,49 @@
+import os
+
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription, OpaqueFunction, GroupAction
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.actions import DeclareLaunchArgument, SetEnvironmentVariable
+from launch.substitutions import LaunchConfiguration
+from launch_ros.parameter_descriptions import ParameterFile
+from launch_ros.actions import Node, PushRosNamespace
+from ament_index_python.packages import get_package_share_directory
+import xacro
+
+def launch_setup(context, *args, **kwargs):
+    pkg_dir = get_package_share_directory("missions_pkg")
+    urdf = os.path.join(pkg_dir, 'launch', 'rhodon', 'rhodon_urdf.xml')
+    namespace = LaunchConfiguration('namespace').perform(context)
+
+    # URDF model (TFs)
+    robot_desc = xacro.process_file(os.path.join(pkg_dir, 'launch', 'rhodon', 'rhodon.xacro'), mappings={'frame_ns': namespace})
+    robot_desc = robot_desc.toprettyxml(indent='  ')
+    robot_state_publisher = [
+        Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            name='robot_state_publisher',
+            output='screen',
+            parameters=[{'robot_description': robot_desc}],
+            arguments=[urdf]
+            ),
+    ]
+
+    return [
+        robot_state_publisher
+    ]
+
+
+def generate_launch_description():
+    return LaunchDescription([
+        # Set env var to print messages to stdout immediately
+        SetEnvironmentVariable('RCUTILS_LOGGING_BUFFERED_STREAM', '1'),
+        
+        DeclareLaunchArgument(
+            "log_level",
+            default_value=["info"],  #debug, info
+            description="Logging level",
+            ),
+        DeclareLaunchArgument('namespace', default_value="rhodon"),
+        OpaqueFunction(function = launch_setup)
+    ])
